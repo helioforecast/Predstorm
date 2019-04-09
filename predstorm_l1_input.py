af192ce7a78b6eb26ee026a37d1803408d96e508
cmoestl <christian.moestl@oeaw.ac.at>
2019-04-09 13:30:08 +0200

    added predstorm_l1_input

---
+++ predstorm_l1_input.py
@@ -0,0 +1,12 @@
+# PREDSTORM L1 input parameters file
+# ----------------------------------
+
+# If True, show interpolated data points on the DSCOVR input plot
+showinterpolated = True
+
+# Time interval for both the observed and predicted windDelta T (hours), start with 24 hours here (covers 1 night of aurora)
+deltat = 24
+
+# Time range of training data (in example 4 solar minimum years as training data for 2018) 
+trainstart = '2006-Jan-01 00:00'
+trainend =   '2010-Jan-01 00:00'
